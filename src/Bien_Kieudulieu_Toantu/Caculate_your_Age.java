22e24475709c55c0b6e167b624316fee45219021
myHomework <dinhhoangvcu@gmail.com>
2018-08-14 00:27:19 +0700

    Caculate_your_Age

---
+++ src/Bien_Kieudulieu_Toantu/Caculate_your_Age.java
@@ -0,0 +1,16 @@
+package Bien_Kieudulieu_Toantu;
+
+        import java.util.Calendar;
+        import java.util.Scanner;
+
+public class Caculate_your_Age {
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        int yearOfBirth,age;
+        System.out.print("Enter your year of birth:");
+        yearOfBirth=scanner.nextInt();
+        int year = Calendar.getInstance().get(Calendar.YEAR);
+        age = year - yearOfBirth;
+        System.out.println("Your Age :" + age + " years old");
+    }
+}
