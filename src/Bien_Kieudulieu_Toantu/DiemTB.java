b03c82867d6bc9b6f7c1d6dc7b2e55148c332ee1
myHomework <dinhhoangvcu@gmail.com>
2018-08-13 23:02:00 +0700

    Tinh diem TB mon hoc

---
+++ src/Bien_Kieudulieu_Toantu/DiemTB.java
@@ -0,0 +1,28 @@
+package Bien_Kieudulieu_Toantu;
+
+import java.util.Locale;
+import java.util.Scanner;
+import java.io.FileNotFoundException;
+import java.io.File;
+
+public class DiemTB {
+    public static void main(String[] args) {
+        float vatly;
+        float hoahoc;
+        float sinhhoc;
+        float tb;
+        float tong;
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+        System.out.print("Nhập điểm Lý :");
+        vatly = scanner.nextFloat();
+        System.out.print("Nhập điểm Hóa :");
+        hoahoc = scanner.nextFloat();
+        System.out.print("Nhập điểm Sinh :");
+        sinhhoc = scanner.nextFloat();
+        tb = ((vatly + hoahoc + sinhhoc) / 3);
+        System.out.println("Điểm TB của bạn là :" + tb);
+        tong = (vatly + hoahoc + sinhhoc);
+        System.out.println("Tổng điểm của bạn là :" + tong);
+
+    }
+}
