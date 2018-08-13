394a00600e93ce4acce565bce85ce5a5a45a97b8
myHomework <dinhhoangvcu@gmail.com>
2018-08-13 23:44:44 +0700

    Perimeter_And_Area_of_Circle

---
+++ src/Bien_Kieudulieu_Toantu/Perimeter_And_Area_of_Circle.java
@@ -0,0 +1,19 @@
+package Bien_Kieudulieu_Toantu;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Perimeter_And_Area_of_Circle {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+        final double PI = 3.14;
+        float BK;
+        double Perimeter;
+        double Area;
+        System.out.print("Nhập bán kính:");
+        BK = scanner.nextFloat();
+        Perimeter = 2 * BK * PI;
+        Area = PI * BK * BK;
+        System.out.println("Chu vi và diện tích hình tròn lần lượt là =:" + Perimeter + " và " + Area);
+    }
+}
