3d2dbf6577b38bf8454e060016670b77b6a0033a
myHomework <dinhhoangvcu@gmail.com>
2018-08-13 23:23:39 +0700

    Change_celsius_to_fahrenheit

---
+++ src/Bien_Kieudulieu_Toantu/Change_celsius_to_fahrenheit.java
@@ -0,0 +1,17 @@
+package Bien_Kieudulieu_Toantu;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Change_celsius_to_fahrenheit {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+        float C;
+        float F;
+        System.out.print("Nhập độ C:");
+        C = scanner.nextInt();
+        F = 9*C/5 + 32;
+        System.out.println("Đổi ra độ F =:"+F);
+
+    }
+}
