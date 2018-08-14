77f7a5f385a5a9411b41037293b39a8c021de6b7
myHomework <dinhhoangvcu@gmail.com>
2018-08-14 11:35:21 +0700

    Application caculate "Leap year"

---
+++ src/Cau_lenh_dieu_kien/Leap_Year.java
@@ -0,0 +1,28 @@
+package Cau_lenh_dieu_kien;
+
+import java.util.Scanner;
+
+public class Leap_Year {
+    public static void main(String[] args) {
+        int year;
+        boolean isLeapYear = false;
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the year to be calculated:");
+        year = scanner.nextInt();
+        if(year % 4 == 0){
+            if(year % 100 == 0){
+                if(year % 400 == 0){
+                    isLeapYear = true;
+                }
+            } else {
+                isLeapYear = true;
+            }
+        }
+
+        if(isLeapYear){
+            System.out.printf("%d is a leap year", year);
+        } else {
+            System.out.printf("%d is NOT a leap year", year);
+        }
+    }
+}
