e2ef8b5578a359691b27914474e3c3b5fbb4b3c9
myHomework <dinhhoangvcu@gmail.com>
2018-08-17 20:25:17 +0700

    Employee_salaries

--- src/Cau_lenh_dieu_kien/Employee_salaries.java
+++ src/Cau_lenh_dieu_kien/Employee_salaries.java
@@ -8,16 +8,16 @@ public class Employee_salaries {
         float heSoLuong;
         int soNamLamViec;
         Scanner sc = new Scanner(System.in).useLocale(Locale.US);
-        System.out.println("Nhập hệ số lương :");
+        System.out.print("Nhập hệ số lương :");
         heSoLuong = sc.nextFloat();
-        System.out.println("Nhập số năm làm việc :");
+        System.out.print("Nhập số năm làm việc :");
         soNamLamViec = sc.nextInt();
         if (heSoLuong > 5 || heSoLuong < 0) {
             System.out.println("Hệ số lương phải nằm trong khoảng 1-5");
             heSoLuong = sc.nextFloat();
         }else {
             float luongThang = heSoLuong * 4000000 + soNamLamViec * 500000;
-            System.out.printf("Lương tháng của bạn là :" + (long)luongThang);
+            System.out.print("Lương tháng của bạn là :"+(long)luongThang+"VNĐ");
         }
     }
 }
