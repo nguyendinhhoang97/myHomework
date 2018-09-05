4dbc38724293f249d8c826f3d9d17da2da44571b
HP Probook 6570b <dinhhoangvcu@gmail.com>
2018-09-05 19:42:07 +0700

    Kiểm tra số nguyên tố

---
+++ src/Caulenhlap/TimSoNguyenTo.java
@@ -0,0 +1,30 @@
+package Caulenhlap;
+
+import java.util.Scanner;
+
+public class TimSoNguyenTo {
+    public static void main(String[] args) {
+        System.out.println("Chương trình tìm số nguyên tố");
+        int x;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Nhập số cần kiểm tra:");
+        x = scanner.nextInt();
+        if (x < 2)
+            System.out.println(x + " không phải số nguyên tố");
+        else {
+            int i = 2;
+            boolean check = true;
+            while (i <= Math.sqrt(x)) {
+                if (x % i == 0) {
+                    check = false;
+                    break;
+                }
+                i++;
+            }
+            if (check == true)
+                System.out.println(x + " là số nguyên tố");
+            else
+                System.out.println(x + " không là số nguyên tố");
+        }
+    }
+}
