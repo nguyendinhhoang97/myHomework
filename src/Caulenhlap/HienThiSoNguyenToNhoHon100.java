f9febd1ebe1ec32895fbc1d4a251f1c4c3eb0e03
HP Probook 6570b <dinhhoangvcu@gmail.com>
2018-09-07 19:44:41 +0700

    Hien thi cac so nguyen to nho hon 100

---
+++ src/Caulenhlap/HienThiSoNguyenToNhoHon100.java
@@ -0,0 +1,23 @@
+package Caulenhlap;
+
+public class HienThiSoNguyenToNhoHon100 {
+    public static void main(String[] args) {
+        System.out.println("Cac so nguyen to nho hon 100 la:");
+        for (int number = 2; number <= 100; number++) {
+            boolean check = true;
+            int i = 2;
+            while (i<=Math.sqrt(number)){
+                if (number % i == 0) {
+                    check = false;
+                    break;
+                }
+                i++;
+            }
+            if (check)
+                System.out.println(number);
+            else
+                continue;
+        }
+
+    }
+}
