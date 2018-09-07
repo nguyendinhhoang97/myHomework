d2bd92c48d65d4d6ecc74c0d9f2afd77c5be92e3
HP Probook 6570b <dinhhoangvcu@gmail.com>
2018-09-07 20:57:25 +0700

    Hien thi 20 so nguyen to dau tien

---
+++ src/Caulenhlap/HienThi20SoNguyenToDauTien.java
@@ -0,0 +1,30 @@
+package Caulenhlap;
+
+public class HienThi20SoNguyenToDauTien {
+    public static void main(String[] args) {
+        System.out.println("20 số nguyên tố đầu tiên là:");
+        int count = 0;
+        int i = 2;
+        while (count < 20) {
+            if (isPrimeNumber(i)) {
+                System.out.print(i + " ");
+                count++;
+            }
+            i++;
+        }
+    }
+
+
+    public static boolean isPrimeNumber(int n) {
+        if (n < 2) {
+            return false;
+        }
+        int squareRoot = (int) Math.sqrt(n);
+        for (int i = 2; i <= squareRoot; i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
